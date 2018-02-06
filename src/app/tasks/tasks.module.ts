b43c7ececa960829196d3110b03aa5c3474649ea
ss.kovalevskiy <ss.kovalevskiy@yandex.ru>
2018-02-06 05:21:31 +0300

    Step 10: Подключаем TasksRoutingModule в массив imports TasksModule

--- src/app/tasks/tasks.module.ts
+++ src/app/tasks/tasks.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {FormsModule} from "@angular/forms";
 import { TaskListComponent, TaskComponent, TaskArrayService } from '.';
+import {TasksRoutingModule} from "./tasks.routing.module";
 
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    TasksRoutingModule
   ],
   declarations: [TaskListComponent, TaskComponent],
   providers: [TaskArrayService]
