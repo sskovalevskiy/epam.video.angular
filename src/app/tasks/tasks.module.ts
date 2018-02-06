9c183af62c7021dfea787e5d815f65e0af014117
ss.kovalevskiy <ss.kovalevskiy@yandex.ru>
2018-02-06 04:34:13 +0300

    Step 08: Создали компонент TaskComponent

--- src/app/tasks/tasks.module.ts
+++ src/app/tasks/tasks.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import {FormsModule} from "@angular/forms";
 import { TaskArrayService } from './services/task-array.service';
 import { TaskListComponent } from './task-list/task-list.component';
+import { TaskComponent } from './task/task.component';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule
   ],
-  declarations: [TaskListComponent],
+  declarations: [TaskListComponent, TaskComponent],
   providers: [TaskArrayService]
 })
 export class TasksModule { }
