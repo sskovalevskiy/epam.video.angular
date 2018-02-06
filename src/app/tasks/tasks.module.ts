5c2d703e9657587d94115e11d05b822346aaaaee
ss.kovalevskiy <ss.kovalevskiy@yandex.ru>
2018-02-06 04:09:45 +0300

    Step 08: Добавил TaskArrayService для работы с задачами - добавления/обновления/завершения задачи

--- src/app/tasks/tasks.module.ts
+++ src/app/tasks/tasks.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {FormsModule} from "@angular/forms";
+import { TaskArrayService } from './services/task-array.service';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule
   ],
-  declarations: []
+  declarations: [],
+  providers: [TaskArrayService]
 })
 export class TasksModule { }
