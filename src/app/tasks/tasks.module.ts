99b9d5e1047a1ddf4c3972e24d87455d6db95a7b
ss.kovalevskiy <ss.kovalevskiy@yandex.ru>
2018-02-06 04:42:53 +0300

    Step 08: Упрощаем импорты в TasksModule благодаря tasks/index.ts

--- src/app/tasks/tasks.module.ts
+++ src/app/tasks/tasks.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {FormsModule} from "@angular/forms";
-import { TaskArrayService } from './services/task-array.service';
-import { TaskListComponent } from './task-list/task-list.component';
-import { TaskComponent } from './task/task.component';
+import { TaskListComponent, TaskComponent, TaskArrayService } from '.';
 
 @NgModule({
   imports: [
