df123ce83d35184f0910be0c0335011c60ecc6c9
ss.kovalevskiy <ss.kovalevskiy@yandex.ru>
2018-02-08 12:43:49 +0300

    Step 17: Создали UserComponent

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -2,13 +2,14 @@ import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {CommonModule} from '@angular/common';
 import { UserArrayService } from './services/user-array.service';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
   ],
-  declarations: [],
+  declarations: [UserComponent],
   providers: [UserArrayService]
 })
 export class UsersModule {
