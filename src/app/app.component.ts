dc71575730580291d226b702f015d600cf80bc4b
ss.kovalevskiy <ss.kovalevskiy@yandex.ru>
2018-02-08 04:58:28 +0300

    Step 16: Добавили конструктор и метод displayMessages() в AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import {Component} from '@angular/core';
+import {MessagesService} from "./core/services/messages.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -7,11 +9,18 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
 
+  constructor(public messagesService: MessagesService, private router: Router) { }
+
   onActivate($event) {
     console.log('Activated Component', $event);
   }
 
   onDeactivate($event) {
     console.log('Deactivated Component', $event);
+  }
+
+  displayMessages(): void {
+    this.router.navigate([{ outlets: { popup: ['messages'] } }]);
+    this.messagesService.isDisplayed = true;
   }
 }
