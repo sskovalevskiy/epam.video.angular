d3b111974200576febd53a6612c9ccf400e0c055
ss.kovalevskiy <ss.kovalevskiy@yandex.ru>
2018-02-08 12:53:39 +0300

    Step 17: Добавили index.ts в папку users, отредактировали импорты в UsersModule

---
+++ src/app/users/index.ts
@@ -0,0 +1,4 @@
+export * from './services/user-array.service';
+export * from './user/user.component';
+export * from './user-form/user-form.component';
+export * from './user-list/user-list.component';
--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -1,8 +1,7 @@
 import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {CommonModule} from '@angular/common';
-import { UserArrayService } from './services/user-array.service';
-import { UserComponent } from './user/user.component';
+import { UserComponent, UserArrayService } from '.';
 
 @NgModule({
   imports: [
@@ -10,6 +9,7 @@ import { UserComponent } from './user/user.component';
     FormsModule,
   ],
   declarations: [UserComponent],
+
   providers: [UserArrayService]
 })
 export class UsersModule {
